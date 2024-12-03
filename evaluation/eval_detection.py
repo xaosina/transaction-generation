e9e2f8639efb95330a3827c7138bcdb2c9cdf2fc
PetrMokrov <mokrov.pv@phystech.edu>
2024-12-03 11:18:11 +0000

    merge

--- evaluation/eval_detection.py
+++ evaluation/eval_detection.py
@@ -72,6 +72,7 @@ def run_eval_detection(
             match_users=match_users,
             save_path=temp_dir,
         )
+        breakpoint()
         return run_model(
             dataset=dataset,
             method=method,
