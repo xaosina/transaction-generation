76ae903097db8a31201f522d4448d4c781512943
xxraytz <egorsurkov.ds@gmail.com>
2025-05-26 10:49:03 +0000

    Add loader transform for quantile-based filtering

--- generation/data/loader_tfs.py
+++ generation/data/loader_tfs.py
@@ -48,8 +48,8 @@ class ExcludeCategories(LoaderTransform):
 
     def __call__(self, data: pd.DataFrame) -> pd.DataFrame:
         data[self.feature_name] = data[self.feature_name].apply(self.remove_excluded)
-        data['_seq_len'] = data[self.feature_name].map(len)
-        mask = data['_seq_len'] >= self.min_len
+        data["_seq_len"] = data[self.feature_name].map(len)
+        mask = data["_seq_len"] >= self.min_len
         return data.loc[mask].reset_index(drop=True)
 
     def new_dataconf(self, data_conf: DataConfig) -> DataConfig:
@@ -59,3 +59,34 @@ class ExcludeCategories(LoaderTransform):
             - len(self.exclude_categories)
         )
         return new_data_conf
+
+
+@dataclass
+class NumericalFilter(LoaderTransform):
+    feature_name: str
+    min_hist_len: int
+    gen_len: int
+    low_quantile: float = 0.01
+    high_quantile: float = 0.99
+    min_cut: bool = True
+    max_cut: bool = True
+
+    def cut(self, data: pd.DataFrame, func) -> pd.DataFrame:
+        s = data["amount"].apply(func)
+        lo, hi = s.quantile([self.low_quantile, self.high_quantile])
+        return data[s.between(lo, hi)]
+
+    def __post_init__(self):
+        self.min_len = self.min_hist_len + self.gen_len
+
+    def __call__(self, data: pd.DataFrame) -> pd.DataFrame:
+        breakpoint()
+        if self.min_cut:
+            data = self.cut(data, min)
+        if self.max_cut:
+            data = self.cut(data, max)
+
+        return data
+
+    def new_dataconf(self, data_conf: DataConfig) -> DataConfig:
+        return data_conf
