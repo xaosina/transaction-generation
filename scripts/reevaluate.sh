d20abeec5136c64f71358c9588c54281bb27403d
xaosina <dima.tina2013@gmail.com>
2025-07-09 14:43:30 +0000

    Added LocalShift - permutation that allows to move no more than max_shift from initial position

--- scripts/reevaluate.sh
+++ scripts/reevaluate.sh
@@ -2,7 +2,7 @@
 
 EXPERIMENT_ROOT="$1"
 RUN_NAME="${2:-evaluation_run}"
-DEVICE="${3:-cuda:0}"
+DEVICE="${3:-cuda:2}"
 
 CONFIG_FILE="${EXPERIMENT_ROOT}/seed_0/config.yaml"
 CHECKPOINT_DIR="${EXPERIMENT_ROOT}/seed_0/ckpt/"
@@ -17,7 +17,7 @@ CHECKPOINT_FILE=$(ls -t "$CHECKPOINT_DIR"*.ckpt | head -n1)
 python main.py \
   --config_path "${CONFIG_FILE}" \
   --trainer.ckpt_resume "${CHECKPOINT_FILE}" \
-  --config_factory [mbd/evaluate] \
+  --config_factory [dataset/mbd/metric/evaluate] \
   --run_name "${RUN_NAME}" \
   --device "${DEVICE}" \
   --runner.params.n_runs 1
